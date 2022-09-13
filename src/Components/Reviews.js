184e860fdc4064a5ee0c579d5eee6ebfb6a3f2ba
Miriam Anyango <minaoanya@gmail.com>
2022-09-13 11:48:13 +0300

    fetching from the database to populate the reviews section

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,9 +1,28 @@
 import './reviews.css';
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 function Reviews () {
+    const [reviews, setReviews] = useState([])
+  
+    useEffect(()=>{
+      fetch('https://mwalimustudentpage.herokuapp.com/reviews')
+      .then(res => res.json())
+      .then((data)=> setReviews(data))
+    },[])
   return (
-    <div>Reviews</div>
+  <div>
+    {reviews.map((review) => (
+    <div key={review.id}>
+      <div className='myreviews'>
+      <div>
+        <p>{review.mentor_id}</p>
+        <p>{review.score}</p>
+        <p>{review.comment}</p>
+        <p>{review.student_id}</p>
+      </div>
+      </div>
+    </div>))}
+  </div>
   )
 }
 export default Reviews;
